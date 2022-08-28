e4ecb4bc13926dc24b76107b4c04d6e54d00d273
yespapa32 <112172608+yespapa32@users.noreply.github.com>
2022-08-28 14:38:58 +0700

    Update Sugar.sh

--- Sugar.sh
+++ Sugar.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer -a yespowerSUGAR -o stratum+tcp://yespowerSUGAR.na.mine.zergpool.com:6535 -u D9BqMzyXr8dTKZs6JDV4PN6iRQKJ6vup79 -p c=DOGE,mc=SUGAR,m=solo -t1
+./cpuminer -a yespowerSUGAR -o stratum+tcp://yespowerSUGAR.na.mine.zergpool.com:6535 -u D9BqMzyXr8dTKZs6JDV4PN6iRQKJ6vup79 -p c=DOGE,mc=SUGAR,m=solo -t2
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
 while [ 1 ]; do
 sleep 3
